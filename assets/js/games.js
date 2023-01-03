dd394a796e418f9b44bcf345192a74c1adcb53f0
Nikola Ćosin <nikola.cosin123@gmail.com>
2023-01-03 18:26:03 +0100

    added data loading feature

---
+++ assets/js/games.js
@@ -0,0 +1,36 @@
+const main = document.querySelector("main");
+let games = [];
+
+const getGames = async () => {
+    const res = await fetch("http://localhost:3000/games");
+    const data = await res.json();
+    return data;
+}
+    await getGames()
+    .then(res => {
+        games = [...res];
+    });
+
+console.log(games);
+
+const writeData = () => {
+    for(let game of games) {
+        main.innerHTML += 
+        `<div class="game">
+            <div class="overlay">
+                <button>Kupi kartu</button>
+            </div>
+            <img src="${game.img}" alt="">
+            <div class="game-text">
+                <h1>${game.team1} - ${game.team2}</h1>
+                <h2>${game.arena}</h2>
+                <span>${game.date}</span>
+                <span>${game.time}</span>
+            </div>
+        </div>`
+    }
+}
+
+writeData();
+
+
